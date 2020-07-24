8bf3c4d72864c76f8bdd62bb872277626dd445d9
Anney Park <anneypark@gmail.com>
2020-07-24 04:03:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,9 +107,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-    
-  programmer_hash[:alan_kay][:known_for] = alans_new_info
   alans_new_info = "GUI"
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
   programmer_hash()
 
 end
