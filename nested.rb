2df99cb4fb4f42b801155c6697190c853317bfe6
Anney Park <anneypark@gmail.com>
2020-07-24 04:01:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -81,13 +82,16 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+  programmer_hash = {:yukihiro_matsumoto => {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"] }
+  }
 end
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
   # return the entire updated hash
-
+  
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -103,6 +107,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
+  programmer_hash[:alan_kay][:known_for] = GUI
 
 end
 
